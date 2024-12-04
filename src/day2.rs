330e981829224a909d72b5d5da0b793653d80453
Trystan May <trystanmay@gmail.com>
2024-12-03 21:04:55 -0500

    Formatted files

--- src/day2.rs
+++ src/day2.rs
@@ -1,23 +1,20 @@
+use crate::utils::*;
 use itertools::{self, Itertools};
 use std::path::Path;
-use crate::utils::*;
-
 
 pub fn main() {
-   let path = Path::new("data/day2-input.txt");
+    let path = Path::new("data/day2-input.txt");
 
-   if let Ok(lines) = read_lines(&path) {
+    if let Ok(lines) = read_lines(&path) {
         let num_safe: usize = lines
             .flatten()
             .map(|line| {
                 line.split_whitespace()
-                    .map(|n| { 
-                        match n.parse::<i64>() {
-                            Ok(num) => num,
-                            Err(e) => panic!("Could not parse integer. Error is {e}"),
-                        }
+                    .map(|n| match n.parse::<i64>() {
+                        Ok(num) => num,
+                        Err(e) => panic!("Could not parse integer. Error is {e}"),
                     })
-                .collect()
+                    .collect()
             })
             .map(|line: Vec<i64>| {
                 line.as_slice()
@@ -26,21 +23,14 @@ pub fn main() {
                     .collect_vec()
             })
             .filter(|diffs: &Vec<i64>| {
-                (diffs.iter().all(|d| *d > 0) || diffs.iter().all(|d| *d < 0)) &&
-                    (diffs.iter().map(|d| d.abs()).fold(true, |acc, next| acc && (next >= 1) && (next <= 3)))
-            }).count();
-//        println!("Safe lines are {num_safe:?}");
+                (diffs.iter().all(|d| *d > 0) || diffs.iter().all(|d| *d < 0))
+                    && (diffs
+                        .iter()
+                        .map(|d| d.abs())
+                        .fold(true, |acc, next| acc && (next >= 1) && (next <= 3)))
+            })
+            .count();
+        //        println!("Safe lines are {num_safe:?}");
         println!("The number of safe lines are {num_safe}");
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
