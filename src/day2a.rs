068817829a6ad1075bc11a4ff778bd6eb69ec2b9
Trystan May <trystanmay@gmail.com>
2024-12-03 22:18:37 -0500

    Formatted

--- src/day2a.rs
+++ src/day2a.rs
@@ -1,7 +1,6 @@
+use crate::utils::*;
 use itertools::{self, Itertools};
 use std::path::Path;
-use crate::utils::*;
-
 
 fn num_mistakes(diffs: &Vec<i64>) -> i64 {
     let mut out = 0;
@@ -19,39 +18,36 @@ fn num_mistakes(diffs: &Vec<i64>) -> i64 {
             if curr_dir != 0 {
                 direction = curr_dir;
             }
-        } else if !( diff.abs() >= 1 && diff.abs() <= 3 ) {
+        } else if !(diff.abs() >= 1 && diff.abs() <= 3) {
             out += 1;
             println!("Out of bounds");
         }
     }
 
     // let num_wrong_direction = (diffs.len() as i64) - diffs.iter().map(|&d| i64::signum(d)).sum::<i64>().abs();
-    // let num_not_in_range = diffs.iter().map(|d| d.abs()).filter(|&d| (d < 1) || (d > 3)).count() as i64; 
+    // let num_not_in_range = diffs.iter().map(|d| d.abs()).filter(|&d| (d < 1) || (d > 3)).count() as i64;
     // let out = diffs.as_slice().windows(2).filter(|dd| (dd[0] < dd[1])  // num_wrong_direction + num_not_in_range;
     println!("Num Mistakes = {out}");
     // println!("Wrong dir = {num_wrong_direction}");
     // println!("Not in range = {num_not_in_range}");
     println!("Differences = {diffs:?}");
     println!("============");
-    out 
+    out
 }
 
-
 pub fn main() {
-   let path = Path::new("data/day2-input.txt");
+    let path = Path::new("data/day2-input.txt");
 
-   if let Ok(lines) = read_lines(&path) {
+    if let Ok(lines) = read_lines(&path) {
         let num_safe: usize = lines
             .flatten()
             .map(|line| {
                 line.split_whitespace()
-                    .map(|n| { 
-                        match n.parse::<i64>() {
-                            Ok(num) => num,
-                            Err(e) => panic!("Could not parse integer. Error is {e}"),
-                        }
+                    .map(|n| match n.parse::<i64>() {
+                        Ok(num) => num,
+                        Err(e) => panic!("Could not parse integer. Error is {e}"),
                     })
-                .collect()
+                    .collect()
             })
             .map(|line: Vec<i64>| {
                 line.as_slice()
@@ -63,15 +59,4 @@ pub fn main() {
             .count();
         println!("The number of safe lines are {num_safe}");
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
